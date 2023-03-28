6898a5b6b8cd82c47618f59ffe161172c20b8d14
Drew Chase <dcmanproductions@gmail.com>
2023-03-28 12:47:39 -0400

    Added code to handle reading and writing NBT data

--- common/src/main/java/chase/minecraft/architectury/warpmod/mixin/PlayerMixin.java
+++ common/src/main/java/chase/minecraft/architectury/warpmod/mixin/PlayerMixin.java
@@ -1,5 +1,6 @@
-package chase.minecraft.architectury.warpmod.mixin.fabric;
+package chase.minecraft.architectury.warpmod.mixin;
 
+import chase.minecraft.architectury.warpmod.data.Warps;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +13,14 @@ public class PlayerMixin {
 
     @Inject(at = @At("RETURN"), method = "addAdditionalSaveData")
     public void addAdditionalSaveData(CompoundTag compoundTag, CallbackInfo cbi) {
+        ServerPlayer player = (ServerPlayer) ((Object) this);
+        compoundTag.put("warps", Warps.fromPlayer(player).toNBT());
+    }
 
+    @Inject(at = @At("RETURN"), method = "readAdditionalSaveData")
+    public void readAdditionalSaveData(CompoundTag compoundTag, CallbackInfo cbi) {
+        ServerPlayer player = (ServerPlayer) ((Object) this);
+        Warps.fromPlayer(player).fromNBT(compoundTag);
     }
 
 }
