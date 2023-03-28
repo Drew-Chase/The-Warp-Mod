7f942e56554938140393a6832ef49c23a323c664
Drew Chase <dcmanproductions@gmail.com>
2023-03-28 12:55:31 -0400

    Added logger

--- common/src/main/java/chase/minecraft/architectury/warpmod/WarpMod.java
+++ common/src/main/java/chase/minecraft/architectury/warpmod/WarpMod.java
@@ -1,9 +1,12 @@
 package chase.minecraft.architectury.warpmod;
 
 import dev.architectury.event.events.common.CommandRegistrationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class WarpMod {
     public static final String MOD_ID = "warpmod";
+    public static Logger log = LogManager.getLogger("WarpMod");
 
     public static void init() {
         CommandRegistrationEvent.EVENT.register((dispatcher, registry, selection) -> {
