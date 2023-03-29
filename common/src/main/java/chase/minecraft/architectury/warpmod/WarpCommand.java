987e49eb062a66d222022c7605cd7372a23dee2e
Drew Chase <dcmanproductions@gmail.com>
2023-03-28 21:37:34 -0400

    added rename and spawn commands

--- common/src/main/java/chase/minecraft/architectury/warpmod/WarpCommand.java
+++ common/src/main/java/chase/minecraft/architectury/warpmod/WarpCommand.java
@@ -4,28 +4,30 @@ import chase.minecraft.architectury.warpmod.data.Warp;
 import chase.minecraft.architectury.warpmod.data.Warps;
 import chase.minecraft.architectury.warpmod.data.enums.WarpCreationResponseType;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.arguments.EntityArgument;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.*;
+import net.minecraft.server.commands.LocateCommand;
 import net.minecraft.server.commands.SpreadPlayersCommand;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.levelgen.Heightmap;
 
 import java.util.Random;
 
-import static com.mojang.brigadier.arguments.StringArgumentType.string;
 import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
+import static com.mojang.brigadier.arguments.StringArgumentType.string;
+import static com.mojang.brigadier.arguments.BoolArgumentType.bool;
 import static net.minecraft.commands.Commands.argument;
 import static net.minecraft.commands.Commands.literal;
 
 public class WarpCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-
         dispatcher.register(
                 literal("warp")
                         .then(argument("name", string())
@@ -54,6 +56,16 @@ public class WarpCommand {
                                         .executes(ctx -> teleportToRandom(ctx, IntegerArgumentType.getInteger(ctx, "max")) ? 1 : 0)
                                 )
                                 .executes(ctx -> teleportToRandom(ctx) ? 1 : 0)
+                        ).then(literal("rename")
+                                .then(argument("old", string())
+                                        .then(argument("new", string())
+                                                .executes(ctx -> renameWarp(ctx, StringArgumentType.getString(ctx, "old"), StringArgumentType.getString(ctx, "new"), false) ? 1 : 0)
+                                        ).then(argument("overwrite", bool())
+                                                .executes(ctx -> renameWarp(ctx, StringArgumentType.getString(ctx, "old"), StringArgumentType.getString(ctx, "new"), BoolArgumentType.getBool(ctx, "overwrite")) ? 1 : 0)
+                                        )
+                                )
+                        ).then(literal("spawn")
+                                .executes(ctx -> teleportToSpawn(ctx) ? 1 : 0)
                         )
         );
     }
@@ -81,6 +93,31 @@ public class WarpCommand {
     }
 
     /**
+     * If the command is run by a player, teleport them to the spawn point
+     *
+     * @param context The context of the command. This is used to get the source of the command, the arguments, and the
+     * command itself.
+     * @return A boolean
+     */
+    private static boolean teleportToSpawn(CommandContext<CommandSourceStack> context) {
+        if (context.getSource().isPlayer()) {
+            ServerPlayer player = context.getSource().getPlayer();
+            if (player == null) {
+                context.getSource().sendFailure(Component.literal("Player was not found!"));
+                return false;
+            }
+            Warp.createBack(player);
+
+            BlockPos pos = player.getLevel().getSharedSpawnPos();
+            player.teleportTo(player.getServer().overworld(), pos.getX() + .5, pos.getY(), pos.getZ() + .5, player.getXRot(), player.getYRot());
+            context.getSource().sendSuccess(Component.literal(String.format("%sWarped to: %sSpawn", ChatFormatting.GREEN, ChatFormatting.GOLD)), false);
+            return true;
+        }
+        context.getSource().sendFailure(Component.literal("Command needs to be run as player"));
+        return false;
+    }
+
+    /**
      * "This function teleports the player to a random location within 100 blocks of their current location."
      * <p>
      * The first line of the function is the function's signature. It tells us the function's name, the type of data it
@@ -90,7 +127,7 @@ public class WarpCommand {
      * @return A boolean value.
      */
     private static boolean teleportToRandom(CommandContext<CommandSourceStack> context) {
-        return teleportToRandom(context, 100);
+        return teleportToRandom(context, 500);
     }
 
     /**
@@ -129,9 +166,20 @@ public class WarpCommand {
     private static boolean teleportToRandom(CommandContext<CommandSourceStack> context, int maxDistance, int minDistance) {
         if (context.getSource().isPlayer()) {
             ServerPlayer player = context.getSource().getPlayer();
-            int dist = new Random().nextInt(minDistance, maxDistance);
-            player.randomTeleport(dist, player.getLevel().getLogicalHeight(), dist, false);
+            if (player == null) {
+                context.getSource().sendFailure(Component.literal("Player was not found!"));
+                return false;
+            }
+            int dist = Warp.teleportRandom(player, maxDistance, minDistance);
+            boolean success = dist != 0;
+            if (!success) {
+                context.getSource().sendFailure(Component.literal("Failed to find a safe place to land."));
+            } else {
+                context.getSource().sendSuccess(Component.literal(String.format("%sTeleported %s%d%s blocks away", ChatFormatting.GREEN, ChatFormatting.GOLD, dist, ChatFormatting.GREEN)), false);
+            }
+            return success;
         }
+        context.getSource().sendFailure(Component.literal("Command must be run as a player."));
         return false;
     }
 
@@ -148,7 +196,11 @@ public class WarpCommand {
     private static boolean createWarp(CommandContext<CommandSourceStack> context, String name) {
         if (context.getSource().isPlayer()) {
             ServerPlayer player = context.getSource().getPlayer();
-            Warp warp = Warp.create(name, player.getBlockX() + .5f, player.getBlockY(), player.getBlockZ() + .5f, player.getYRot(), player.getXRot(), player);
+            if (player == null) {
+                context.getSource().sendFailure(Component.literal("Player was not found!"));
+                return false;
+            }
+            Warp warp = Warp.create(name, player.getBlockX(), player.getBlockY(), player.getBlockZ(), player.getYRot(), player.getXRot(), player, false);
             WarpCreationResponseType response = Warps.fromPlayer(player).createAddOrUpdate(warp);
             if (response == WarpCreationResponseType.FailureDueToDuplicate) {
                 context.getSource().sendFailure(Component.literal(String.format("Warp already exists with name: %s", name)));
@@ -166,6 +218,44 @@ public class WarpCommand {
         } else {
             context.getSource().sendFailure(Component.literal("Command has to be run as a Player!"));
         }
+        return false;
+    }
+
+    /**
+     * If the command is being run by a player, and the warp exists, and the new name doesn't exist, or the new name exists
+     * and we're overwriting, then rename the warp
+     *
+     * @param context The context of the command.
+     * @param name The name of the warp to rename
+     * @param new_name The new name of the warp
+     * @param overwrite If the new name already exists, should it be overwritten?
+     * @return A boolean value.
+     */
+    private static boolean renameWarp(CommandContext<CommandSourceStack> context, String name, String new_name, boolean overwrite) {
+        if (context.getSource().isPlayer()) {
+            ServerPlayer player = context.getSource().getPlayer();
+            if (player == null) {
+                context.getSource().sendFailure(Component.literal("Player was not found!"));
+                return false;
+            }
+            Warps warps = Warps.fromPlayer(player);
+            if (!warps.Exists(name)) {
+                context.getSource().sendFailure(Component.literal("Warp does NOT exist!"));
+                return false;
+            }
+            if (warps.Exists(new_name)) {
+                if (!overwrite) {
+                    context.getSource().sendFailure(Component.literal(String.format("Warp already exists: %s%s", ChatFormatting.GOLD, new_name)));
+                    return false;
+                }
+            }
+            Warp old = warps.Get(name);
+            Warp.create(new_name, old.getX(), old.getY(), old.getZ(), old.getYaw(), old.getPitch(), player, true);
+            context.getSource().sendSuccess(Component.literal(String.format("%sWarp renamed %s%s %s-> %s%s", ChatFormatting.GREEN, ChatFormatting.GOLD, name, ChatFormatting.GOLD, ChatFormatting.GREEN, new_name)), false);
+            return true;
+        }
+
+        context.getSource().sendFailure(Component.literal("Command needs to be run as a Player!"));
         return false;
     }
 
@@ -215,9 +305,11 @@ public class WarpCommand {
      */
     private static boolean listWarps(CommandContext<CommandSourceStack> context, ServerPlayer player) {
         Warps warps = Warps.fromPlayer(player);
-        MutableComponent component = Component.literal(String.format("%sList:%s\n", ChatFormatting.GOLD, ChatFormatting.WHITE));
+        MutableComponent component = Component.literal("");
         for (Warp warp : warps.GetWarps()) {
-            component.append(String.format("%s%s:%s [x: %d, y: %d, z: %d, pitch: %d, yaw: %d]%s - %sDIM: %s,\n", ChatFormatting.AQUA, warp.getName(), ChatFormatting.LIGHT_PURPLE, (int) warp.getX(), (int) warp.getY(), (int) warp.getZ(), (int) warp.getPitch(), (int) warp.getYaw(), ChatFormatting.WHITE, ChatFormatting.GREEN, warp.getLevelResourceLocation().getPath()));
+            component.append(Component.literal(String.format("%s%s: ", ChatFormatting.GOLD, warp.getName())));
+            component.append(ComponentUtils.wrapInSquareBrackets(Component.translatable("chat.coordinates", warp.getX(), warp.getY(), warp.getZ())).withStyle((style) -> style.withColor(ChatFormatting.GREEN).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format("/warp %s", warp.getName()))).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal(String.format("Click to Warp to %s", warp.getName()))))));
+            component.append(Component.literal(String.format(" %sDIM: %s\n", ChatFormatting.GOLD, warp.getLevelResourceLocation().getPath())));
         }
         context.getSource().sendSuccess(component, false);
         return true;
