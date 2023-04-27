18ad45e86be6e429dd23d74eb090319ca467fc0f
Drew Chase <dcmanproductions@gmail.com>
2023-04-26 23:44:09 -0400

    added post and pre render overlay for forge

--- forge/src/main/java/chase/minecraft/architectury/warpmod/forge/ClientWarpModForge.java
+++ forge/src/main/java/chase/minecraft/architectury/warpmod/forge/ClientWarpModForge.java
@@ -2,18 +2,22 @@ package chase.minecraft.architectury.warpmod.forge;
 
 import chase.minecraft.architectury.warpmod.WarpMod;
 import chase.minecraft.architectury.warpmod.client.WarpModClient;
+import chase.minecraft.architectury.warpmod.client.gui.GUIFactory;
 import net.minecraft.network.Connection;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@Mod.EventBusSubscriber(modid = WarpMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = WarpMod.MOD_ID, bus = EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientWarpModForge extends WarpModClient
 {
 	@SubscribeEvent
-	static void clientTick(TickEvent.ClientTickEvent event)
+	public static void clientTick(final TickEvent.ClientTickEvent event)
 	{
 		if (event.phase == TickEvent.Phase.END)
 		{
@@ -22,19 +26,39 @@ public class ClientWarpModForge extends WarpModClient
 	}
 	
 	@SubscribeEvent
-	static void logginIn(ClientPlayerNetworkEvent.LoggingIn event)
+	public static void loggingIn(final ClientPlayerNetworkEvent.LoggingIn event)
 	{
 		Connection connection = event.getConnection();
-		if (connection != null)
-			onServerLogin(connection);
+		onServerLogin(connection);
 	}
 	
 	@SubscribeEvent
-	static void logginOut(ClientPlayerNetworkEvent.LoggingOut event)
+	public static void loggingOut(final ClientPlayerNetworkEvent.LoggingOut event)
 	{
-		Connection connection = event.getConnection();
-		if (connection != null)
-			onServerLogout(connection);
+		if (event.getConnection() != null)
+			onServerLogout(event.getConnection());
 	}
+	
+	@SubscribeEvent
+	static void changeDimension(final EntityTravelToDimensionEvent event)
+	{
+		if (event.getEntity() instanceof ServerPlayer player)
+		{
+			WarpModClient.changeDimension(player, event.getDimension().location());
+		}
+	}
+	
+	@SubscribeEvent
+	static void renderOverlay(final RenderGuiOverlayEvent.Pre event)
+	{
+		GUIFactory.PreGameOverlay(event.getPoseStack());
+	}
+	
+	@SubscribeEvent
+	static void renderOverlay(final RenderGuiOverlayEvent.Post event)
+	{
+		GUIFactory.PostGameOverlay(event.getPoseStack());
+	}
+	
 	
 }
