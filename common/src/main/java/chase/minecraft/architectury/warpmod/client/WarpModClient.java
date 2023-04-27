e3fa66918d74f2c41fef5243b43df4b74bd3aecc
Drew Chase <dcmanproductions@gmail.com>
2023-04-26 23:51:33 -0400

    added change dimension event, WARP_DIRECTORY, added vanilla dimensions by default

--- common/src/main/java/chase/minecraft/architectury/warpmod/client/WarpModClient.java
+++ common/src/main/java/chase/minecraft/architectury/warpmod/client/WarpModClient.java
@@ -3,29 +3,39 @@ package chase.minecraft.architectury.warpmod.client;
 import chase.minecraft.architectury.warpmod.data.Warps;
 import chase.minecraft.architectury.warpmod.networking.ClientNetworking;
 import chase.minecraft.architectury.warpmod.networking.WarpNetworking;
+import chase.minecraft.architectury.warpmod.utils.WorldUtils;
+import com.google.common.collect.ImmutableList;
+import dev.architectury.platform.Platform;
 import io.netty.buffer.Unpooled;
 import lol.bai.badpackets.api.PacketSender;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.network.Connection;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public abstract class WarpModClient
 {
 	
 	@NotNull
-	public static String[] dimensions = {"minecraft:overworld", "minecraft:the_nether", "minecraft:the_end"};
+	public static List<String> dimensions = ImmutableList.of("minecraft:overworld", "minecraft:the_nether", "minecraft:the_end");
 	public static boolean onServer = false;
 	public static boolean isOP = false;
 	public static String remoteVersion = "";
+	public static final Path WARP_DIRECTORY = Path.of(Platform.getGameFolder().toString(), "The Warp Mod");
 	
 	/**
 	 * Initializes the client
 	 */
 	public static void init()
 	{
+		WARP_DIRECTORY.toFile().mkdirs();
 		initKeyBindings();
 		ClientNetworking.init();
 	}
@@ -63,6 +73,7 @@ public abstract class WarpModClient
 		data.writeBoolean(true);
 		PacketSender.c2s().send(WarpNetworking.PING, data);
 		Warps.loadClient();
+		
 	}
 	
 	protected static void onServerLogout(Connection connection)
@@ -70,5 +81,13 @@ public abstract class WarpModClient
 		Warps.saveClient();
 	}
 	
+	public static void changeDimension(ServerPlayer player, ResourceLocation dimension)
+	{
+		WorldUtils.removeTravelBar(player);
+		if (!WarpModClient.dimensions.contains(dimension.toString()))
+		{
+			WarpModClient.dimensions.add(dimension.toString());
+		}
+	}
 	
 }
