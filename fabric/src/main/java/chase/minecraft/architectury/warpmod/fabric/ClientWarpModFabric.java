cee4168d57020aca8137cbea2580767d8aeae07a
Drew Chase <dcmanproductions@gmail.com>
2023-05-04 21:41:29 -0400

    replaced platform specific to architectury

--- fabric/src/main/java/chase/minecraft/architectury/warpmod/fabric/ClientWarpModFabric.java
+++ fabric/src/main/java/chase/minecraft/architectury/warpmod/fabric/ClientWarpModFabric.java
@@ -2,8 +2,6 @@ package chase.minecraft.architectury.warpmod.fabric;
 
 import chase.minecraft.architectury.warpmod.client.WarpModClient;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
 public class ClientWarpModFabric extends WarpModClient implements ClientModInitializer
 {
@@ -13,9 +11,6 @@ public class ClientWarpModFabric extends WarpModClient implements ClientModIniti
 	@Override
 	public void onInitializeClient()
 	{
-		WarpModClient.init();
-		ClientTickEvents.END_CLIENT_TICK.register(client -> onClientTick());
-		ClientPlayConnectionEvents.JOIN.register((listener, sender, minecraft) -> onServerLogin(listener.getConnection()));
-		ClientPlayConnectionEvents.DISCONNECT.register((listener, minecraft) -> onServerLogout(listener.getConnection()));
+		init();
 	}
 }
--- forge/src/main/java/chase/minecraft/architectury/warpmod/forge/ClientWarpModForge.java
+++ forge/src/main/java/chase/minecraft/architectury/warpmod/forge/ClientWarpModForge.java
@@ -3,50 +3,14 @@ package chase.minecraft.architectury.warpmod.forge;
 import chase.minecraft.architectury.warpmod.WarpMod;
 import chase.minecraft.architectury.warpmod.client.WarpModClient;
 import chase.minecraft.architectury.warpmod.client.gui.GUIFactory;
-import net.minecraft.network.Connection;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @EventBusSubscriber(modid = WarpMod.MOD_ID, bus = EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientWarpModForge extends WarpModClient
 {
-	@SubscribeEvent
-	public static void clientTick(final TickEvent.ClientTickEvent event)
-	{
-		if (event.phase == TickEvent.Phase.END)
-		{
-			onClientTick();
-		}
-	}
-	
-	@SubscribeEvent
-	public static void loggingIn(final ClientPlayerNetworkEvent.LoggingIn event)
-	{
-		Connection connection = event.getConnection();
-		onServerLogin(connection);
-	}
-	
-	@SubscribeEvent
-	public static void loggingOut(final ClientPlayerNetworkEvent.LoggingOut event)
-	{
-		if (event.getConnection() != null)
-			onServerLogout(event.getConnection());
-	}
-	
-	@SubscribeEvent
-	static void changeDimension(final EntityTravelToDimensionEvent event)
-	{
-		if (event.getEntity() instanceof ServerPlayer player)
-		{
-			WarpModClient.changeDimension(player, event.getDimension().location());
-		}
-	}
 	
 	@SubscribeEvent
 	static void renderOverlay(final RenderGuiOverlayEvent.Pre event)
